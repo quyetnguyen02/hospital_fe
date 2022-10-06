3f5f395b816af7779a4dff873bf258a323c373c4
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-10-06 21:20:23 +0700

    push code header and banner

---
+++ src/components/header/header.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="header-top">
+    <div class="container">
+      <div class="d-flex align-items-center">
+        <div class="logo">
+          <img src="https://laptop88.vn/media/banner/logo_logo882022.png" alt="">
+        </div>
+        <div class="header-mid d-flex align-items-center">
+          <div class="form-input">
+            <form action="">
+              <div class="d-flex input-search">
+                <input type="text" placeholder="search..." class="text-search">
+                <button class="submit-search">
+                  <i class="bi bi-search"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+          <div class="list-header d-flex align-items-center">
+            <a href="" class="item d-flex align-items-center">
+              <i class="icon feedback"></i>
+              <p class="text">Góp ý <br> khiếu nại</p>
+            </a>
+            <a href="" class="item d-flex align-items-center">
+              <i class="icon insurance"></i>
+              <p class="text">Tra cứu <br> bảo hành </p>
+            </a>
+            <a href="" class="item d-flex align-items-center">
+              <i class="icon contact"></i>
+              <p class="text">Liên hệ <br> trực tiếp </p>
+            </a>
+            <a href="" class="item d-flex align-items-center">
+              <i class="icon support"></i>
+              <p class="text">Tư vấn <br> mua hàng </p>
+            </a>
+          </div>
+        </div>
+        <div class="header-right">
+          <a href="" class="item">Chính sách</a>
+          <a href="" class="item">Tuyển dụng</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="header-bottom">
+    <div class="container">
+      <div class="content-header-bottom d-flex align-items-center">
+        <div class="header-menu">
+          <div class="title d-flex">
+            <i class="icon category"></i>
+            <p>DANH SÁCH SẢN PHẨM</p>
+          </div>
+          <div class="menu_holder">
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_laptop.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_buy.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_pc.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_maytinhdeban.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/manhinhmt.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_update.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_tracuubaohanh.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+            <div class="item">
+              <a href="" class="item-category d-flex align-items-center">
+                <div class="item-cate-left d-flex align-items-center">
+                  <div class="img-cate">
+                    <img src="https://laptop88.vn/template/giaodien_2022/images/ic_chothuelaptop.png" alt="">
+                  </div>
+                  <p class="cate-title m-0">Laptop Mới</p>
+                </div>
+                <div class="item-cate-right">
+                  <i class="bi bi-caret-right-fill"></i>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="header-product-hot d-flex align-items-center">
+          <a href="" class="item-hot d-flex align-items-center">
+            <div class="pulse-icon">
+              <div class="icon-swap"></div>
+              <div class="elements">
+                <div class="pulse pulse-1"></div>
+              </div>
+            </div>
+            <div class="content">
+              <p class="m-0">HP Envy 2022 Rẻ Nhất TT chỉ 15 triệu</p>
+            </div>
+          </a>
+          <a href="" class="item-hot d-flex align-items-center">
+            <div class="pulse-icon">
+              <div class="icon-swap"></div>
+              <div class="elements">
+                <div class="pulse pulse-1"></div>
+              </div>
+            </div>
+            <div class="content">
+              <p class="m-0">HP Envy 2022 Rẻ Nhất TT chỉ 15 triệu</p>
+            </div>
+          </a>
+          <a href="" class="item-hot d-flex align-items-center">
+            <div class="pulse-icon">
+              <div class="icon-swap"></div>
+              <div class="elements">
+                <div class="pulse pulse-1"></div>
+              </div>
+            </div>
+            <div class="content">
+              <p class="m-0">HP Envy 2022 Rẻ Nhất TT chỉ 15 triệu</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import "@/assets/css/header/header.css"
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,123 @@
 <template>
-  <div class="class">
-    <p>home</p>
+  <Header/>
+  <div>
+    <main>
+      <div class="homepage">
+        <div class="container">
+          <div class="banner d-flex">
+            <div class="banner-left">
+              <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel-vertical">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="https://laptop88.vn/media/banner/30_Aug6dd884036dd7995327265a1235d2ba26.jpg" class="w-100 h-100 opacity-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://laptop88.vn/media/banner/17_Auga810929ba41c71971f6809121a9d1c85.jpg" class="w-100 h-100 opacity-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://laptop88.vn/media/banner/01_Oct66e7bbe76886a0509b0596e39a9d6dd4.jpg" class="w-100 h-100 opacity-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://laptop88.vn/media/banner/20_Aug6a288c920639f66f37ee06e382786736.jpg" class="w-100 h-100 opacity-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://laptop88.vn/media/banner/01_Octe44718f8ec60f64ac2b5f1aeaaa00bac.jpg" class="w-100 h-100 opacity-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <i class="bi bi-caret-left-fill icon-prev" aria-hidden="true"></i>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <i class="bi bi-caret-right-fill icon-next" aria-hidden="true"></i>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+            <div class="right-address">
+              <div class="js-address-showroom">
+                <div class="cv-carousel cv-drag cv-loaded">
+                  <div class="cv-nav">
+                    <div class="cv-prev">
+                      <i class="bi bi-caret-up-fill icon-up" aria-hidden="true"></i>
+                    </div>
+                    <div class="cv-next">
+                      <i class="bi bi-caret-down-fill icon-down" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                  <div class="cv-stage-outer">
+                    <div class="cv-stage">
+                      <div class="cv-item">
+                        <div class="item">
+                          <b>Hà Nội - 125 Trần Đại Nghĩa</b>
+                          <a href="https://zalo.me/0904583588" class="phone">Zalo: 0904.583.588</a>
+                        </div>
+                      </div>
+                      <div class="cv-item">
+                        <div class="item">
+                          <b>Hà Nội - 125 Trần Đại Nghĩa</b>
+                          <a href="https://zalo.me/0904583588" class="phone">Zalo: 0904.583.588</a>
+                        </div>
+                      </div>
+                      <div class="cv-item">
+                        <div class="item">
+                          <b>Hà Nội - 125 Trần Đại Nghĩa</b>
+                          <a href="https://zalo.me/0904583588" class="phone">Zalo: 0904.583.588</a>
+                        </div>
+                      </div>
+                      <div class="cv-item">
+                        <div class="item">
+                          <b>Hà Nội - 125 Trần Đại Nghĩa</b>
+                          <a href="https://zalo.me/0904583588" class="phone">Zalo: 0904.583.588</a>
+                        </div>
+                      </div>
+                      <div class="cv-item">
+                        <div class="item">
+                          <b>Hà Nội - 125 Trần Đại Nghĩa</b>
+                          <a href="https://zalo.me/0904583588" class="phone">Zalo: 0904.583.588</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div class="banner-sale d-flex">
+            <a href="" class="item">
+              <img src="https://laptop88.vn/media/banner/30_Sep41a5ed76c8b372fa66bccefa6396233e.png" alt="">
+            </a>
+            <a href="" class="item">
+              <img src="https://laptop88.vn/media/banner/04_Aug4a47a0db6e60853dedfcfdf08a5ca249.png" alt="">
+            </a>
+            <a href="" class="item">
+              <img src="https://laptop88.vn/media/banner/22_Augf00e444b2224c0103be1aed83629aff9.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
+<script>
+import Header from "@/components/header/header.vue";
+import * as Bootstrap from 'bootstrap';
+import "@/assets/css/home.css";
+
+export default {
+  mounted(){
+    var myCarousel = document.querySelector('#myCarousel')
+    new Bootstrap.Carousel(myCarousel)
+  },
+
+  components: { Header },
+}
+</script>
