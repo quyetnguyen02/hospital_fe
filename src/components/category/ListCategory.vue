30b2dafe42b1b3cae1d25f926fad5845e75a0ffb
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-10-10 20:39:38 +0700

    push code list product component

---
+++ src/components/category/ListCategory.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="list-cate d-flex flex-wrap justify-content-center">
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+      <a href="" class="item-cate">
+        <img src="https://laptop88.vn/media/lib/17-06-2022/icon_chuot.png" alt="">
+        <p class="txt">Chuột máy tính</p>
+      </a>
+    </div>
+  </div>
+</template>
+<script setup>
+import "@/assets/css/category/listCategory.css";
+</script>
